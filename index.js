2e94c258a137d18665881b16d763ccab2ae57dbf
dmfenton <dfenton@esri.com>
2017-03-29 16:14:17 -0400

    remove unneeded line in index

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const pkg = require('./package.json')
 const provider = {
   name: 'googlesheets',
   hosts: true,
-  disableIdParam: true,
   Model: require('./sheets'),
   version: pkg.version,
   type: 'provider'
